9f40e125be57e82a8333bafffb64f455dbf2b100
Ruben Lago <>
2021-09-10 21:27:58 +0200

    inyectado cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -23,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/productos', productosRouter)
