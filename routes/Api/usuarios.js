a0a5be87d8c2c6319a52a0f3f018382f8eee0514
Ruben Lago <>
2021-09-15 11:10:49 +0200

    encriptacion password ok

--- routes/Api/usuarios.js
+++ routes/Api/usuarios.js
@@ -1,11 +1,13 @@
 const { crearUsuario, borrarUsuarioId } = require('../../models/usuarios.models');
-
-
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
 //resgistrar nuevo usario
 router.post('/registro', async (req, res) => {
     //console.log(req.body)
+
+    req.body.password = bcrypt.hashSync(req.body.password, 12) //password encryptadas
+
     const nuevoUsuario = await crearUsuario(req.body)
     res.json(nuevoUsuario)
 
